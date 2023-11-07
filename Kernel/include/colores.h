ac5d708490121a200e27183236dccec3fe69d1de
valenontivero <vontivero@itba.edu.ar>
2023-11-07 18:21:04 -0300

    colores.h

---
+++ Kernel/include/colores.h
@@ -0,0 +1,14 @@
+#ifndef COLORES_H
+#define COLORES_H
+
+typedef struct Color {
+    char r, g, b;
+} Color;
+
+static Color RED = {0xFF, 0, 0};
+static Color GREEN = {0, 0xFF, 0};
+static Color BLUE = {0, 0, 0xFF};
+static Color WHITE = {0xFF, 0xFF, 0xFF};
+static Color YELLOW = {0xFF, 0xFF, 0};
+
+#endif
